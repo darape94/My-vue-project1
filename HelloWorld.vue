3ed9b6a649bd36008c625ac54f01ff70683debc6
darape94 <chandrad0807@gmail.com>
2026-02-09 15:13:17 -0600

    Create HelloWorld.vue

---
+++ HelloWorld.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+const message: string = "Hello from Vue + TypeScript!";
+</script>
+
+<template>
+  <div class="hello">
+    <h2>{{ message }}</h2>
+  </div>
+</template>
+
+<style scoped>
+.hello {
+  padding: 20px;
+  border: 1px solid #ddd;
+}
+</style>
