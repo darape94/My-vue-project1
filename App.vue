37bf50016eea89c55229bc18179be9e48e04eaa4
darape94 <chandrad0807@gmail.com>
2026-02-09 15:14:00 -0600

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <main>
+    <h1>My Vue + TypeScript Project</h1>
+    <HelloWorld />
+  </main>
+</template>
+
+<style>
+main {
+  font-family: Arial, sans-serif;
+  padding: 40px;
+}
+</style>
