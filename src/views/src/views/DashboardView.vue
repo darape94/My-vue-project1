bf89199c0489d97d2ca99f27af9298c67c4a8753
darape94 <chandrad0807@gmail.com>
2026-02-09 15:25:15 -0600

    Create DashboardView.vue
    Use TaskCard component in dashboard view

---
+++ src/views/src/views/DashboardView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import TaskCard from "../components/TaskCard.vue";
+import type { Task } from "../types/task";
+
+const tasks: Task[] = [
+  {
+    id: 1,
+    title: "Design dashboard UI",
+    description: "Create layout and components",
+    status: "In Progress",
+    priority: "High",
+  },
+];
+</script>
+
+<template>
+  <section>
+    <h2>Dashboard</h2>
+
+    <TaskCard
+      v-for="task in tasks"
+      :key="task.id"
+      :task="task"
+    />
+  </section>
+</template>
